d12401caced42eb9f7b80e02efd2dd05f34d023c
ad-ahanadutta <ahanadutta555@gmail.com>
2020-10-07 19:18:19 +0530

    Updates

---
+++ precache-manifest.dafef3e86fc6ba5a5cd7dfe81f4f1725.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d06cb089fab1cddee2f7ffcfd09caca6",
+    "url": "/GALLERY/index.html"
+  },
+  {
+    "revision": "ae1f98a24759d6c5cc76",
+    "url": "/GALLERY/static/css/main.93220237.chunk.css"
+  },
+  {
+    "revision": "28a2766a9a476910dc34",
+    "url": "/GALLERY/static/js/2.097a7464.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/GALLERY/static/js/2.097a7464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae1f98a24759d6c5cc76",
+    "url": "/GALLERY/static/js/main.bcbe6293.chunk.js"
+  },
+  {
+    "revision": "2aa086704189e3e38e41",
+    "url": "/GALLERY/static/js/runtime-main.89702e01.js"
+  }
+]);
